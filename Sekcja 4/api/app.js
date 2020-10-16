8e60ee2fd460f857a47f241c6b3fafdf6782c8ce
Fremen1990 <fremen1990@gmail.com>
2020-10-16 02:12:58 +0200

    Sekcja 4 API

--- Sekcja 4/api/app.js
+++ Sekcja 4/api/app.js
@@ -7,6 +7,13 @@ const fetch = require('node-fetch');
 // console.log(process.argv)
 
 const year = process.argv[2];
-console.log(year)
+// console.log(year)
 
-fetch('http://numbersapi.com/1990/year?hson')
+fetch(`http://numbersapi.com/1990/${year}?json`)
+    .then(response => {
+        console.log(response.status);
+        console.log(response.ok);
+        return response.json()
+    })
+    .then(data => console.log(data.text))
+    .catch(error => console.log("eeerrrorrrryyy", error))
