29ce8e599c5f07be8ec24d939cdfa2e39796e320
Fremen1990 <fremen1990@gmail.com>
2020-11-08 02:35:00 +0100

    second commit

--- Sekcja 6 EXPRESS/Podstawy EXPRESS/app.js
+++ Sekcja 6 EXPRESS/Podstawy EXPRESS/app.js
@@ -8,6 +8,10 @@ app.listen(9000, () => {
 
 app.all('/', (req) => {
     // console.log("Welcome Traveller")
-    console.log(req.method)
+    console.log("req.url", req.url);
+    console.log("req.originalUrl", req.originalUrl);
+    console.log("req.path", req.path)
+    console.log("req.protocol", req.protocol)
+    console.log("req.secure", req.secure)
 })
 
