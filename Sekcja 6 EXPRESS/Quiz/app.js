2d57d861e2a33f308579cd8bbaaf11a94b6eb28e
Fremen1990 <fremen1990@gmail.com>
2020-11-10 00:36:46 +0100

    QUIZ part 2

--- Sekcja 6 EXPRESS/Quiz/app.js
+++ Sekcja 6 EXPRESS/Quiz/app.js
@@ -1,11 +1,40 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
 
-app.listen(4000, () => {
-    console.log("You are listening on port http://localhost:4000")
+
+app.listen(port, () => {
+    console.log(`You are listening on port http://localhost:${port}`)
 })
+
+let goodAnswers = 0;
+let callToFriendUsed = false;
+let quesionToTheCrowdUsed = false;
+let halfOnHalfUsed = false;
+
+const questions = [{
+    question: "What is the best programming language?",
+    answers: ["C++", "Fortran", "JavaScrupt", "Java"],
+    correctAnswer: 0,
+},
+{
+    question: "Is this course cool?",
+    answers: ["No", "I dont know", "Have no idea", "YES!!"],
+    correctAnswer: 3,
+},
+{
+    question: "Do you wanna pizza??",
+    answers: ["Yes", "no!!", "yammy", "no way! im dieting!"],
+    correctAnswer: 2,
+
+}
+
+]
+
+
 
 app.get('/', (req, res) => {
     res.send("WORKING!!")
 })
+
