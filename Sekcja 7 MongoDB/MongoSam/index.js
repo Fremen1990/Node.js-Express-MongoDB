32466740e0969da7951a758ede0a257f9dc70008
Fremen1990 <fremen1990@gmail.com>
2020-11-12 00:34:27 +0100

    mongo commit

--- Sekcja 7 MongoDB/MongoSam/index.js
+++ Sekcja 7 MongoDB/MongoSam/index.js
@@ -12,8 +12,31 @@ client.connect(err => {
 
         const cars = db.collection('cars')
 
-        cars.deleteOne({ _id: mongo.ObjectID("5fac6e97f0d6e3516f39b8a6") })
+        // cars.deleteOne({ _id: mongo.ObjectID("5fac6e97f0d6e3516f39b8a6") })
 
+        cars.updateMany({
+
+            brand: "Saab",
+
+        }, {
+            $set: {
+                model: "900",
+            }
+        }, err => {
+            if (err) {
+                console.log("error during actualllizaaaation")
+            } else { console.log("all is fine") }
+        }
+
+        )
+
+        // cars.find({}).toArray((err, dataCars) => {
+        //     if (err) {
+        //         console.log("wrong query!! ")
+        //     } else {
+        //         console.log("Cars: ", dataCars)
+        //     }
+        // })
 
 
         client.close();
